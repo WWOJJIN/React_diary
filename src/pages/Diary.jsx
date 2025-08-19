58cd866d6b0684c369fe5d0e1bbad82db8c5185b
Woojin <119497359+WWOJJIN@users.noreply.github.com>
2025-08-19 12:18:18 +0900

    추

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
 const Diary = () => {
     const { id } = useParams
+    
     return (
         <div>{id} Diary</div>
     )
